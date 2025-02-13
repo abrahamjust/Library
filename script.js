52e86bff41bba748aae8ebeb7ebdcef647e936f2
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2025-02-13 14:30:19 +0000

    created the skeleton code

---
+++ script.js
@@ -0,0 +1,9 @@
+// array to store the books
+const myLibrary = [];
+
+// constructor for the book object
+function book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.status = status;
+}
